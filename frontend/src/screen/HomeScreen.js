5cb55d608cf4058232ff1c2a853ebe28f96dc21d
adithya <adithyanuzpratamaputra@gmail.com>
2021-05-22 10:06:34 +0700

    fixing

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -18,10 +18,9 @@ function HomeScreen(props) {
                 <div className="col-1">
                     <div className="banner">
                         <h1>Virtual Lab<span> Biology</span></h1>
-                        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
-                            dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
-                            Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
-                            sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
+                        <p>Virtual laboratory merupakan media berbasis komputer yang dapat digunakan untuk peserta didik 
+                            dalam melakukan percobaan serta dapat mebambah semangat dan memotivasi peserta didik. Penggunaan virtual laboratory dapat menghemat waktu, 
+                            serta pembelajaran lebih menarik dan proses pembelajaran dapat berlangsung kapanpun dan dimanapun dilakukan.
                         </p>                          
                         <div className="btn">
                             <Link to="/choose">
